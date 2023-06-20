f9fbae0cee4a4f87a0b90ad6daf70491917aa82c
Harry Nakos <xnakos@gmail.com>
2023-06-20 14:34:42 +0300

    Send GStreamer data received on internal socket to all active clients on external socket.

--- server.cpp
+++ server.cpp
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <poll.h>
 #include <unistd.h>
 #include <iostream>
 #include <set>
@@ -22,7 +23,7 @@ std::set<sockaddr_in, sockaddr_in_cmp> client_sockaddrs;
 
 void dispatch()
 {
-    // External socket that clients send to
+    // Socket that external clients send to
     int socket_ext = socket(AF_INET, SOCK_DGRAM, 0);
     if (socket_ext < 0)
     {
@@ -32,7 +33,7 @@ void dispatch()
 
     sockaddr_in sockaddr_ext{};
     sockaddr_ext.sin_family = AF_INET;
-    sockaddr_ext.sin_addr.s_addr = INADDR_ANY;
+    inet_pton(AF_INET, "0.0.0.0", &(sockaddr_ext.sin_addr));
     sockaddr_ext.sin_port = htons(PORT_EXT);
 
     if (bind(socket_ext, (struct sockaddr *)&sockaddr_ext, sizeof(sockaddr_ext)) < 0)
@@ -41,7 +42,7 @@ void dispatch()
         return;
     }
 
-    // Internal socket that GStreamer sends to
+    // Socket that internal GStreamer sends to
     int socket_int = socket(AF_INET, SOCK_DGRAM, 0);
     if (socket_int < 0)
     {
@@ -51,55 +52,95 @@ void dispatch()
 
     sockaddr_in sockaddr_int{};
     sockaddr_int.sin_family = AF_INET;
-    sockaddr_int.sin_addr.s_addr = INADDR_LOOPBACK;
+    inet_pton(AF_INET, "127.0.0.1", &(sockaddr_int.sin_addr));
     sockaddr_int.sin_port = htons(PORT_INT);
 
+    if (bind(socket_int, (struct sockaddr *)&sockaddr_int, sizeof(sockaddr_int)) < 0)
+    {
+        std::cerr << "Failed to bind socket_int." << std::endl;
+        return;
+    }
+
+    struct pollfd fds[2];
+
+    // TODO Check whether using the same buffer for both sockets is OK and whether it should be outside of the loop
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read;
+    // TODO Same check for these
     sockaddr_in client_sockaddr{};
     socklen_t client_sockaddr_len = sizeof(client_sockaddr);
 
+    fds[0].fd = socket_ext;
+    fds[0].events = POLLIN;
+    fds[1].fd = socket_int;
+    fds[1].events = POLLIN;
+
     while (true)
     {
-        // Receive from client
-        bytes_read = recvfrom(socket_ext, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_sockaddr, &client_sockaddr_len);
-        if (bytes_read < 0)
+        // Block until a socket event occurs
+        int poll_res = poll(fds, 2, -1);
+        if (poll_res < 0)
         {
-            std::cerr << "Failed to receive." << std::endl;
-            continue;
+            std::cerr << "Poll error." << std::endl;
+            return;
         }
 
-        // Print sending client info
-        char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(client_sockaddr.sin_addr), client_ip, INET_ADDRSTRLEN);
-        std::cout << "Received from client: " << client_ip << ":" << ntohs(client_sockaddr.sin_port) << std::endl;
-
-        // Add client to active clients, if client is not already there.
-        if (client_sockaddrs.count(client_sockaddr) == 0)
+        // Check whether socket_ext has data
+        if (fds[0].revents & POLLIN)
         {
-            client_sockaddrs.insert(client_sockaddr);
-        }
-
-        // Send received data to all active clients
-        // FIXME Do not use socket_int
-        for (const auto &client_sockaddr : client_sockaddrs)
-        {
-            if (sendto(socket_int, buffer, bytes_read, 0, (struct sockaddr *)&client_sockaddr, sizeof(client_sockaddr)) < 0)
+            // Receive from client
+            bytes_read = recvfrom(socket_ext, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_sockaddr, &client_sockaddr_len);
+            if (bytes_read < 0)
             {
-                std::cerr << "Failed to send." << std::endl;
+                std::cerr << "Failed to receive from client." << std::endl;
                 continue;
+            }
+            // TODO Remove this and buffer printing. This is for printing only and will break things if the buffer is full.
+            buffer[bytes_read] = '\0';
+            std::cout << "Received data on socket_ext: '" << buffer << "' from " << inet_ntoa(client_sockaddr.sin_addr) << ":" << ntohs(client_sockaddr.sin_port) << std::endl;
+
+            // Add client to active clients, if client is not already there.
+            if (client_sockaddrs.count(client_sockaddr) == 0)
+            {
+                client_sockaddrs.insert(client_sockaddr);
+            }
+        }
+
+        // Check whether socket_int has data
+        if (fds[1].revents & POLLIN)
+        {
+            // Receive from GStreamer
+            // TODO Check whether the same buffer and struct should be used for GStreamer
+            bytes_read = recvfrom(socket_int, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_sockaddr, &client_sockaddr_len);
+            if (bytes_read < 0)
+            {
+                std::cerr << "Failed to receive from GStreamer." << std::endl;
+                continue;
+            }
+            // TODO Remove this and buffer printing. This is for printing only and will break things if the buffer is full.
+            buffer[bytes_read] = '\0';
+            std::cout << "Received data on socket_int: '" << buffer << "' from " << inet_ntoa(client_sockaddr.sin_addr) << ":" << ntohs(client_sockaddr.sin_port) << std::endl;
+
+            // Send received data from GStreamer to all active clients
+            for (const auto &client_sockaddr : client_sockaddrs)
+            {
+                if (sendto(socket_ext, buffer, bytes_read, 0, (struct sockaddr *)&client_sockaddr, sizeof(client_sockaddr)) < 0)
+                {
+                    std::cerr << "Failed to send to client." << std::endl;
+                    continue;
+                }
             }
         }
 
         // Print active clients
-        std::cout << "-------- Active clients --------" << std::endl;
+        std::cout << "---- Active clients ----" << std::endl;
         for (const auto &client_sockaddr : client_sockaddrs)
         {
             char client_ip[INET_ADDRSTRLEN];
             inet_ntop(AF_INET, &(client_sockaddr.sin_addr), client_ip, INET_ADDRSTRLEN);
             std::cout << client_ip << ":" << ntohs(client_sockaddr.sin_port) << std::endl;
         }
-        std::cout << "--------------------------------" << std::endl;
+        std::cout << "------------------------" << std::endl;
     }
 
     close(socket_ext);
