6f35f6684c923025bedea6ea1d1824e3d93270a2
Harry Nakos <xnakos@gmail.com>
2023-07-10 22:17:47 +0300

    Add a GStreamer pipeline to the server with appsrc input and appsink output that currently flips the video horizontally.

--- server.cpp
+++ server.cpp
@@ -1,12 +1,19 @@
 #include <arpa/inet.h>
+#include <gst/gst.h>
+#include <gst/app/gstappsrc.h>
+#include <gst/app/gstappsink.h>
 #include <poll.h>
 #include <unistd.h>
+#include <chrono>
 #include <iostream>
 #include <set>
 #include <thread>
 
-const int BUFFER_SIZE = 1024;
+// TODO Check whether this should be higher
+const int BUFFER_SIZE = 4096;
+// TODO Rename this to PORT, after the other one below it is discarded
 const int PORT_EXT = 62000;
+// TODO Discard this and all related functionality
 const int PORT_INT = 63000;
 
 struct sockaddr_in_cmp
@@ -21,14 +28,71 @@ struct sockaddr_in_cmp
 
 std::set<sockaddr_in, sockaddr_in_cmp> client_sockaddrs;
 
-void dispatch()
+/**
+ * Get data from the `appsink` at the end of the GStreamer pipeline and
+ * send it to all active clients using the supplied socket.
+ *
+ * TODO Check whether using a callback for "new-sample" is better than
+ * using a separate thread and blocking on `gst_app_sink_pull_sample`.
+ */
+void route(GstAppSink *appsink, int socket_ext)
 {
+    while (true)
+    {
+        GstSample *sample = gst_app_sink_pull_sample(appsink);
+        GstBuffer *buffer = gst_sample_get_buffer(sample);
+        GstMapInfo info;
+
+        if (gst_buffer_map(buffer, &info, GST_MAP_READ))
+        {
+            const guint8 *buffer_data = info.data;
+            const gsize buffer_size = info.size;
+
+            // Send data to all active clients
+            // TODO Protect for the case of a simultaneous write
+            for (const auto &client_sockaddr : client_sockaddrs)
+            {
+                if (sendto(socket_ext, buffer_data, buffer_size, 0, (struct sockaddr *)&client_sockaddr, sizeof(client_sockaddr)) < 0)
+                {
+                    std::cerr << "Failed to send to client." << std::endl;
+                    continue;
+                }
+            }
+
+            gst_buffer_unmap(buffer, &info);
+        }
+
+        gst_sample_unref(sample);
+    }
+}
+
+int main()
+{
+    // Initialize GStreamer
+    gst_init(nullptr, nullptr);
+
+    // Processing pipeline description (currently only flips horizontally)
+    const char *processing_pipeline_desc = "appsrc name=appsrc caps=\"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96\" ! rtph264depay ! decodebin ! videoscale ! videoconvert ! videoflip method=horizontal-flip ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! appsink name=appsink";
+
+    // Create processing pipeline
+    GstElement *processing_pipeline = gst_parse_launch(processing_pipeline_desc, nullptr);
+
+    // Get the appsrc element of the processing pipeline
+    GstElement *appsrc = gst_bin_get_by_name(GST_BIN(processing_pipeline), "appsrc");
+    // The following is for a live timestamped stream
+    g_object_set(G_OBJECT(appsrc), "stream-type", 0, "is-live", TRUE, "format", GST_FORMAT_TIME, nullptr);
+
+    // Get the appsink element of the processing pipeline
+    GstElement *appsink = gst_bin_get_by_name(GST_BIN(processing_pipeline), "appsink");
+
+    // TODO Almost everything above is not cleaned up afterwards
+
     // Socket that external clients send to
     int socket_ext = socket(AF_INET, SOCK_DGRAM, 0);
     if (socket_ext < 0)
     {
         std::cerr << "Failed to create socket_ext." << std::endl;
-        return;
+        return 1;
     }
 
     sockaddr_in sockaddr_ext{};
@@ -39,7 +103,7 @@ void dispatch()
     if (bind(socket_ext, (struct sockaddr *)&sockaddr_ext, sizeof(sockaddr_ext)) < 0)
     {
         std::cerr << "Failed to bind socket_ext." << std::endl;
-        return;
+        return 1;
     }
 
     // Socket that internal GStreamer sends to
@@ -47,7 +111,7 @@ void dispatch()
     if (socket_int < 0)
     {
         std::cerr << "Failed to create socket_int." << std::endl;
-        return;
+        return 1;
     }
 
     sockaddr_in sockaddr_int{};
@@ -58,7 +122,7 @@ void dispatch()
     if (bind(socket_int, (struct sockaddr *)&sockaddr_int, sizeof(sockaddr_int)) < 0)
     {
         std::cerr << "Failed to bind socket_int." << std::endl;
-        return;
+        return 1;
     }
 
     struct pollfd fds[2];
@@ -75,6 +139,12 @@ void dispatch()
     fds[1].fd = socket_int;
     fds[1].events = POLLIN;
 
+    std::thread route_thread(route, GST_APP_SINK(appsink), socket_ext);
+
+    auto start_time = std::chrono::steady_clock::now();
+
+    gst_element_set_state(processing_pipeline, GST_STATE_PLAYING);
+
     while (true)
     {
         // Block until a socket event occurs
@@ -82,7 +152,7 @@ void dispatch()
         if (poll_res < 0)
         {
             std::cerr << "Poll error." << std::endl;
-            return;
+            return 1;
         }
 
         // Check whether socket_ext has data
@@ -95,14 +165,28 @@ void dispatch()
                 std::cerr << "Failed to receive from client." << std::endl;
                 continue;
             }
+
             // TODO Remove this and buffer printing. This is for printing only and will break things if the buffer is full.
-            buffer[bytes_read] = '\0';
-            std::cout << "Received data on socket_ext: '" << buffer << "' from " << inet_ntoa(client_sockaddr.sin_addr) << ":" << ntohs(client_sockaddr.sin_port) << std::endl;
+            // buffer[bytes_read] = '\0';
+            // std::cout << "Received data on socket_ext: '" << buffer << "' from " << inet_ntoa(client_sockaddr.sin_addr) << ":" << ntohs(client_sockaddr.sin_port) << std::endl;
 
             // Add client to active clients, if client is not already there.
             if (client_sockaddrs.count(client_sockaddr) == 0)
             {
                 client_sockaddrs.insert(client_sockaddr);
+            }
+
+            GstBuffer *gst_buffer = gst_buffer_new_allocate(nullptr, bytes_read, nullptr);
+            const auto bytes_copied = gst_buffer_fill(gst_buffer, 0, buffer, bytes_read);
+            auto now_time = std::chrono::steady_clock::now();
+            GST_BUFFER_PTS(gst_buffer) = std::chrono::duration_cast<std::chrono::nanoseconds>(now_time - start_time).count();
+            const auto result = gst_app_src_push_buffer(GST_APP_SRC(appsrc), gst_buffer);
+
+            if (result != GST_FLOW_OK)
+            {
+                std::cerr << "Failed to push buffer to appsrc." << std::endl;
+                gst_object_unref(processing_pipeline);
+                return 1;
             }
         }
 
@@ -145,12 +229,6 @@ void dispatch()
 
     close(socket_ext);
     close(socket_int);
-}
-
-int main()
-{
-    std::thread dispatch_thread(dispatch);
-    dispatch_thread.join();
 
     return 0;
 }
