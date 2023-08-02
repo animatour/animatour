e92de99029003b5798546f0bfb45a42bdd7f2403
Harry Nakos <xnakos@gmail.com>
2023-08-02 18:29:48 +0300

    Use dynamic ports for everything. Refactoring needed.

--- server.cpp
+++ server.cpp
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <gio/gio.h>
 #include <gst/gst.h>
 #include <poll.h>
 #include <unistd.h>
@@ -28,8 +29,10 @@ std::set<sockaddr_in, sockaddr_in_cmp> client_sockaddrs;
 // Maps client port to GStreamer pipeline udpsrc address
 std::map<u_int, sockaddr_in> map_gst;
 
-// Auto-increments for now
-int port_gst = 5000;
+std::vector<guint16> ports;
+
+// FIXME This is problematic, do not go at it this way. Also take care of removing idle stuff.
+int i = 0;
 
 int main()
 {
@@ -39,7 +42,7 @@ int main()
     // Processing pipeline description (currently only flips horizontally)
     // const char *processing_pipeline_desc = "appsrc name=appsrc caps=\"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96\" ! rtph264depay ! decodebin ! videoscale ! videoconvert ! videoflip method=horizontal-flip ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! appsink name=appsink";
     // Based on `gst-launch-1.0 compositor name=comp sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=320 sink_1::ypos=0 sink_2::xpos=640 sink_2::ypos=0 sink_3::xpos=0 sink_3::ypos=240 ! autovideosink videotestsrc pattern=white ! video/x-raw, framerate=30/1, width=320, height=240 ! comp. videotestsrc pattern=red ! videobox ! video/x-raw, framerate=60/1, width=320, height=240 ! comp. videotestsrc pattern=green ! videobox ! video/x-raw, framerate=30/1, width=320, height=240 ! comp. videotestsrc pattern=blue ! videobox ! video/x-raw, framerate=30/1, width=320, height=240 ! comp.`
-    const char *processing_pipeline_desc = "compositor name=compositor sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=320 sink_1::ypos=0 sink_2::xpos=640 sink_2::ypos=0 sink_3::xpos=0 sink_3::ypos=240 ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink name=udpsink host=127.0.0.1 udpsrc name=udpsrc_0 port=5000 caps=\"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96\" ! rtph264depay ! avdec_h264 ! videoscale ! videoconvert ! video/x-raw, framerate=30/1, width=320, height=240 ! compositor. videotestsrc pattern=red ! videobox ! video/x-raw, framerate=60/1, width=320, height=240 ! compositor. videotestsrc pattern=green ! videobox ! video/x-raw, framerate=30/1, width=320, height=240 ! compositor. udpsrc name=udpsrc_1 port=5001 caps=\"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96\" ! rtph264depay ! avdec_h264 ! videoscale ! videoconvert ! video/x-raw, framerate=30/1, width=320, height=240 ! compositor.";
+    const char *processing_pipeline_desc = "compositor name=compositor sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=320 sink_1::ypos=0 sink_2::xpos=640 sink_2::ypos=0 sink_3::xpos=0 sink_3::ypos=240 ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink name=udpsink host=127.0.0.1 udpsrc name=udpsrc_0 caps=\"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96\" ! rtph264depay ! avdec_h264 ! videoscale ! videoconvert ! video/x-raw, framerate=30/1, width=320, height=240 ! compositor. videotestsrc pattern=red ! videobox ! video/x-raw, framerate=60/1, width=320, height=240 ! compositor. videotestsrc pattern=green ! videobox ! video/x-raw, framerate=30/1, width=320, height=240 ! compositor. udpsrc name=udpsrc_1 caps=\"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96\" ! rtph264depay ! avdec_h264 ! videoscale ! videoconvert ! video/x-raw, framerate=30/1, width=320, height=240 ! compositor.";
 
     // Create processing pipeline
     GstElement *processing_pipeline = gst_parse_launch(processing_pipeline_desc, nullptr);
@@ -110,6 +113,67 @@ int main()
     fds[1].fd = socket_int;
     fds[1].events = POLLIN;
 
+    // Create a new GSocket using the GInetSocketAddress with UDP protocol
+    GSocket *socket = g_socket_new(G_SOCKET_FAMILY_IPV4, G_SOCKET_TYPE_DATAGRAM, G_SOCKET_PROTOCOL_UDP, NULL);
+
+    // Create a GInetSocketAddress with any IP and port 0 to let the system assign a port
+    GSocketAddress *address = G_SOCKET_ADDRESS(g_inet_socket_address_new(g_inet_address_new_any(G_SOCKET_FAMILY_IPV4), 0));
+
+    // Bind the socket to the address
+    if (g_socket_bind(socket, address, TRUE, NULL) == FALSE)
+    {
+        g_object_unref(socket);
+        g_object_unref(address);
+        std::cerr << "Failed to bind the socket to the address." << std::endl;
+        return 1;
+    }
+
+    // Get the assigned port number
+    GSocketAddress *bound_address = g_socket_get_local_address(socket, NULL);
+    GInetSocketAddress *bound_inet_address = G_INET_SOCKET_ADDRESS(bound_address);
+    guint16 port = g_inet_socket_address_get_port(bound_inet_address);
+
+    std::cout << "Socket bound to port: " << port << std::endl;
+
+    GstElement *udpsrc_0 = gst_bin_get_by_name(GST_BIN(processing_pipeline), "udpsrc_0");
+    g_object_set(udpsrc_0, "socket", socket, nullptr);
+
+    ports.push_back(port);
+
+    // Create a new GSocket using the GInetSocketAddress with UDP protocol
+    GSocket *socket2 = g_socket_new(G_SOCKET_FAMILY_IPV4, G_SOCKET_TYPE_DATAGRAM, G_SOCKET_PROTOCOL_UDP, NULL);
+
+    // Create a GInetSocketAddress with any IP and port 0 to let the system assign a port
+    GSocketAddress *address2 = G_SOCKET_ADDRESS(g_inet_socket_address_new(g_inet_address_new_any(G_SOCKET_FAMILY_IPV4), 0));
+
+    // Bind the socket to the address
+    if (g_socket_bind(socket2, address2, TRUE, NULL) == FALSE)
+    {
+        g_object_unref(socket2);
+        g_object_unref(address2);
+        std::cerr << "Failed to bind the socket to the address." << std::endl;
+        return 1;
+    }
+
+    // Get the assigned port number
+    GSocketAddress *bound_address2 = g_socket_get_local_address(socket2, NULL);
+    GInetSocketAddress *bound_inet_address2 = G_INET_SOCKET_ADDRESS(bound_address2);
+    guint16 port2 = g_inet_socket_address_get_port(bound_inet_address2);
+
+    std::cout << "Socket bound to port: " << port2 << std::endl;
+
+    GstElement *udpsrc_1 = gst_bin_get_by_name(GST_BIN(processing_pipeline), "udpsrc_1");
+    g_object_set(udpsrc_1, "socket", socket2, nullptr);
+
+    ports.push_back(port2);
+
+    // Your UDP socket is now ready for use.
+
+    // Don't forget to clean up
+    // g_object_unref(socket);
+    // g_object_unref(bound_address);
+    // g_object_unref(address);
+
     gst_element_set_state(processing_pipeline, GST_STATE_PLAYING);
 
     while (true)
@@ -141,12 +205,13 @@ int main()
                 sockaddr_in sockaddr_udpsrc{};
                 sockaddr_udpsrc.sin_family = AF_INET;
                 inet_pton(AF_INET, "127.0.0.1", &(sockaddr_udpsrc.sin_addr));
-                sockaddr_udpsrc.sin_port = htons(port_gst);
+                sockaddr_udpsrc.sin_port = htons(ports[i]);
                 // FIXME port is not enough, the whole sockaddr is needed, host, port
                 map_gst[client_sockaddr.sin_port] = sockaddr_udpsrc;
-                port_gst++;
+                i++;
             }
 
+            // This is the way to go (maybe without copy), get the address to use with sendto.
             sockaddr_in sockaddr_udpsrc_gst = map_gst[client_sockaddr.sin_port];
 
             if (sendto(socket_ext, buffer, bytes_read, 0, (struct sockaddr *)&sockaddr_udpsrc_gst, sizeof(sockaddr_udpsrc_gst)) < 0)
